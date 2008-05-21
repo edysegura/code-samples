0beaae85e4ccce26cfd17b1f1b2ccba1ca70b735
Edy Segura <edysegura@gmail.com>
2008-05-21 14:39:38 +0000

    Atualização da técnica veja +

---
+++ javascript/tecnicas/veja +/js/Index.js
@@ -0,0 +1,47 @@
+﻿/**
+ *
+ * Script para aplicar a tecnica veja +
+ * @author: Edy Segura - edy@segura.eti.br
+ *
+ */
+
+var Index = {
+
+	init: function() {
+		Index.setBehavior();
+	},
+	
+	
+	setBehavior: function() {
+		var links = document.getElementById("information").getElementsByTagName("a");
+		
+		for(var i=0; i<links.length; i++) {
+			var link = links[i];
+			
+			link.onclick = function() {
+				Index.showInformation(this);
+				return false;
+			}
+		}
+	},
+	
+	
+	showInformation: function(link) {
+		var div = link.parentNode.parentNode;
+		Index.hideInformation();
+		div.className = "show";
+	},
+	
+	
+	hideInformation: function() {
+		var divs = document.getElementById("information").getElementsByTagName("div");
+		
+		for(var i=0; i<divs.length; i++) {
+			divs[i].className = "";
+		}
+	}
+}
+
+
+//inicializacao
+window.onload = Index.init;
