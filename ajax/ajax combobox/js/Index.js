1f9d77e77ecd90e26741cda5136575a97279e7e0
Edy Segura <edysegura@gmail.com>
2008-06-20 15:24:46 +0000

    Atualização do Ajax Combobox

--- ajax/ajax combobox/js/Index.js
+++ ajax/ajax combobox/js/Index.js
@@ -1,7 +1,7 @@
 /**
  *
- * Script para uso do Ajax com combobox
- * @author: Edy Segura - edy@segura.eti.br
+ * Script para uso da metodologia Ajax em combobox
+ * @author: Edy Segura - edy@segura.pro.br
  *
  */
 
@@ -10,73 +10,76 @@ var Index = {
 	init: function() {
 		Index.setForm();
 		Index.setComboUF();
-	},//fim init
+	},
 	
 	
 	setForm: function() {
-		var oForm = document.forms[0];
+		var form = document.forms[0];
 
-		oForm.onsubmit = function() {
+		form.onsubmit = function() {
 			return false;
 		};
 
-	},//fim setForm
+	},
 	
 	
 	setComboUF: function() {
-		var oComboUF = document.getElementById('uf');
+		var comboUF = document.getElementById('uf');
 
-		oComboUF.onchange = function() {
+		comboUF.onchange = function() {
 			Index.buscarCidades(this.value);
 		};
 
-	},//fim setComboUF
+	},
 	
 	
-	buscarCidades: function(sUrl) {
-		if(sUrl) {
-			var oComboCidade = document.getElementById('cidade');
-			var sUrl = "json-estados/" + sUrl.toLowerCase() + ".txt";
+	buscarCidades: function(estado) {
+		var comboCidade = document.getElementById('cidade');
+		
+		if(estado) {
+			var estadoUrl   = "json-estados/" + estado.toLowerCase() + ".txt";
 	
 			//limpando combo cidade
-			oComboCidade.options.length = 0;
-			oComboCidade.options[0] = new Option("-- Aguarde --");
+			comboCidade.options.length = 0;
+			comboCidade.options[0] = new Option("-- Aguarde --");
 	
-			Ajax.run({
-				url      : sUrl,
+			Ajax.request({
+				url      : estadoUrl,
 				callback : Index.mostrarCidades,
 				loading  : true,
-				params   : oComboCidade
+				params   : comboCidade
 			});
+		}
+		else {
+			comboCidade.options.length = 0;
+			comboCidade.options[0] = new Option("-- Selecione o estado --");
+		}
+	},
 	
-		}//fim if
-	},//fim buscarCidades
 	
-	
-	mostrarCidades: function(sResponse, oComboCidade) {
-		var aCidades = eval('(' + sResponse + ')');
+	mostrarCidades: function(response, comboCidade) {
+		var cidades = eval('(' + response + ')');
 		
 		//limpando combo cidade
-		oComboCidade.options.length = 0;
-		Index.setCombo(oComboCidade);
+		comboCidade.options.length = 0;
+		Index.setCombo(comboCidade);
 		
-		aCidades = aCidades.sort();
+		cidades = cidades.sort();
 		
 		//popula o select com a lista de cidades obtida
-		for(var i=0; i<aCidades.length; i++) {
-			aCidades[i] = unescape(aCidades[i]);
-			oComboCidade.options[oComboCidade.options.length] = new Option(aCidades[i], aCidades[i]);
+		for(var i=0; i<cidades.length; i++) {
+			cidades[i] = unescape(cidades[i]);
+			comboCidade.options[comboCidade.options.length] = new Option(cidades[i], cidades[i]);
 		}
-	},//fim getCidades
+	},
 	
 	
-	setCombo: function(oComboCidade) {
-		oComboCidade.options[0] = new Option("Cidades", "", true, true);
-		oComboCidade.options[0].className = "title";
-	}//fim setCombo
+	setCombo: function(comboCidade) {
+		comboCidade.options[0] = new Option("Cidades", "", true, true);
+		comboCidade.options[0].className = "title";
+	}
 	
-};//fim Index
+};
 
-//inicialização
+//inicializacao
 window.onload = Index.init;
-
