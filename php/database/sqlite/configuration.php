eb678ed4c0a336259a914e51d39a921c269ba859
Edy Segura <edysegura@gmail.com>
2008-07-01 12:30:19 +0000

    Atualização do exemplo de SQLite with PDO.

--- php/database/sqlite/configuration.php
+++ php/database/sqlite/configuration.php
@@ -1,8 +1,15 @@
-<?
+<?php
+/**
+ * 
+ * Arquivo de configuracao
+ * @author Edy Segura, edy@segura.pro.br
+ * 
+ */
+
 session_start();
 global $configuration;
 
 //PDO related settings
-$configuration['pdoDriver']= 'sqlite';
-$configuration['sqliteDatabase']= 'test.db';
+$configuration["pdoDriver"]      = "sqlite";
+$configuration["sqliteDatabase"] = "comments.sqlite";
 ?>
