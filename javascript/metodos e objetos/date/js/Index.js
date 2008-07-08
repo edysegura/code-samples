bc28a487d61e206e84eb1b22e6eb1579da638030
Edy Segura <edysegura@gmail.com>
2008-07-08 20:11:14 +0000

    Atualização do exemplo do objeto Date!

--- javascript/metodos e objetos/date/js/Index.js
+++ javascript/metodos e objetos/date/js/Index.js
@@ -2,54 +2,74 @@
  *
  * Objeto Literal Index que exemplifica o uso do
  * objeto Date e seus métodos
- * 
- * Autor: Edy Segura, edy@segura.eti.br
+ * @author: Edy Segura, edy@segura.pro.br
  *
  */
 
 var Index = {
 	
 	init: function() {
+		Index.setShowDate();
+	},
+	
+	setShowDate: function() {
 		Index.showDate();
-	},//fim Init
-	
-	
+		window.setInterval(Index.showDate, 1000);
+	},
+
+
 	showDate: function() {
-		
-		var fnShowDate = function() {
-			var oElementP = document.getElementsByTagName('p')[1];
-			oElementP.innerHTML = Index.getDates();
-		};
-		
-		window.setInterval(fnShowDate, 1000);
-	},//fim showDate
+		var p = document.getElementsByTagName("p")[1];
+		p.innerHTML = Index.getDates();
+	},
 	
 	
 	getDates: function() {
 		return [Index.getFormatDate(), (new Date()).toLocaleString()].join("<br />");
-	},//fim getDates
+	},
 	
 	
 	getFormatDate: function() {
-		var sResult;
+		var result;
 		
-		var oDate  = new Date();          //Data completa.
-		var iDay   = oDate.getDate();     //Dia do mês.
-		var iWeek  = oDate.getDay();      //Dia da semana.
-		var iMonth = oDate.getMonth();    //Mês representado em números.
-		var iYear  = oDate.getFullYear(); //Ano representado em números.
+		var date  = new Date();         //Data completa.
+		var day   = date.getDate();     //Dia do mês.
+		var week  = date.getDay();      //Dia da semana.
+		var month = date.getMonth();    //Mês representado em números.
+		var year  = date.getFullYear(); //Ano representado em números.
 		
-		var aWeek  = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
-		var aMonth = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
-									"Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+		var weekLabel = [
+			"Domingo", 
+			"Segunda", 
+			"Terça", 
+			"Quarta", 
+			"Quinta", 
+			"Sexta", 
+			"Sábado"
+		];
 		
-		iDay    = (iDay < 10) ? "0" + iDay : iDay;
-		sResult = aWeek[iWeek] + ", " + iDay + " de " + aMonth[iMonth] + " de " + iYear + ".";
+		var monthLabel = [
+			"Janeiro", 
+			"Fevereiro", 
+			"Março", 
+			"Abril", 
+			"Maio", 
+			"Junho", 
+			"Julho", 
+			"Agosto", 
+			"Setembro", 
+			"Outubro", 
+			"Novembro", 
+			"Dezembro"
+		];
 		
-		return sResult;
-	}//fim showDate
+		day    = (day < 10) ? "0" + day : day;
+		result = weekLabel[week] + ", " + day + " de " + monthLabel[month] + " de " + year + ".";
+		
+		return result;
+	}
 	
-};//fim Index
+};
 
 //inicializacao
 window.onload = Index.init;
