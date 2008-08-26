fea0c0f714abb2223ddcffe7764742e6baace736
Edy Segura <edysegura@gmail.com>
2008-08-26 19:41:32 +0000

    Adição do Event.target

---
+++ javascript/eventos/event.target/js/Index.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * Mouse.getSource() and Mouse.getPosition()
+ * @author: Edy Segura - edy@segura.pro.br
+ * 
+ */
+
+var Index = {
+
+	init: function() {
+		Index.showTarget();
+	},
+	
+	
+	showTarget: function() {
+		var span = document.getElementById("element");
+		document.onmousemove = function(e) {
+			span.innerHTML = Index.getSource(e).nodeName;
+		}
+	},
+	
+	
+	getSource: function(e) {
+		e = e || window.event;
+		
+		var target = (e.target) ? e.target : 
+		             (e.srcElement) ? e.srcElement : null;
+		
+		//necessario para safari e konqueror
+		if(target && target.nodeType == 3) target = target.parentNode;
+		return target;
+	}
+	
+};
+
+//inicializacao
+window.onload = Index.init;
