b9cfd3d762c8cb764b883a092bc0699e38ebb049
Edy Segura <edysegura@gmail.com>
2008-08-26 14:21:50 +0000

    Imagens para o ajax loading!

---
+++ ajax/ajax-loading/js/Index.js
@@ -0,0 +1,43 @@
+﻿/** 
+ *
+ * Script para carregar as imagens dinamicamente
+ * @author: Edy Segura - edy@segura.pro.br
+ * 
+ */
+
+var Index = {
+	
+	imageCounter: 2,
+	
+	
+	init: function() {
+		Index.getImages();
+	},	
+	
+	
+	getImages: function() {
+		var image = new Image();
+		
+		image.setAttribute("src", "images/loading_" + Index.imageCounter + ".gif");
+		image.setAttribute("alt", "Carregando...");
+		
+		image.onload = function() {
+			var container = document.getElementById("image-container");
+			var p = document.createElement("p");
+			
+			p.appendChild(this);
+			container.appendChild(p);
+			
+			Index.imageCounter++;
+			
+			window.setTimeout(function(){
+				Index.getImages();
+			}, 500);
+			
+		};
+	}
+
+};
+
+//inicializacao
+window.onload = Index.init;
