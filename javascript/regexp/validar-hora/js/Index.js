aa6b29afed169bc60ccd53c8576e09c8588f650f
Edy Segura <edysegura@gmail.com>
2008-08-27 20:09:10 +0000

    Validando hora!

---
+++ javascript/regexp/validar-hora/js/Index.js
@@ -0,0 +1,33 @@
+﻿/**
+ *
+ * Expressão regular para validar hora
+ * @author: Edy Segura - edy@segura.pro.br
+ *
+ */
+
+var Index = {
+
+	init: function() {
+		Index.setInput();
+	},
+	
+	
+	checkHour: function(hour) {
+		var pattern = /^([01][0-9]|2[0-3]):[0-5][0-9]$/;
+		return pattern.test(hour);
+	},
+	
+	
+	setInput: function() {
+		var input = document.getElementById("hours");
+		input.onkeyup = function() {
+			var status = document.getElementById("status");
+			var result = Index.checkHour(this.value);
+			status.innerHTML = (result) ? "Hora válida" : "Hora inválida";
+		}
+	}
+	
+}
+
+//inicializacao
+window.onload = Index.init;
