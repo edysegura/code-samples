1a41bfcf6f55373099d91ea3fc304eb1e495e423
Edy Segura <edysegura@gmail.com>
2008-09-13 23:57:15 +0000

    Adição do banco de dados access

--- php/database/PDO/connectdb.inc.php
+++ php/database/PDO/connectdb.inc.php
@@ -8,6 +8,7 @@
 
 try { 
 	//$pdo = new PDO("pgsql:host=localhost;dbname=users", "root", "root");
+	//$pdo = new PDO("odbc:users", "", ""); //access
 	//$pdo = new PDO("firebird:dbname=localhost:d:\\www\\usersdb\\usuarios.gdb", "SYSDBA", "masterkey");
 	//$pdo = new PDO("mysql:host=localhost;dbname=users", "root", "root");
 	$pdo = new PDO("sqlite:usuarios.sqlite");
