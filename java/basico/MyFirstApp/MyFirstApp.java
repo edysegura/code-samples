21b5c0a9410c900033fee4726678d327a88d0637
Edy Segura <edysegura@gmail.com>
2008-10-17 17:22:04 +0000

    atualização

--- java/basico/MyFirstApp/MyFirstApp.java
+++ java/basico/MyFirstApp/MyFirstApp.java
@@ -1,8 +1,7 @@
 public class MyFirstApp {
 
 	public static void main (String[] args) {
-		//saida no console
-		System.out.println("I'm Edy Segura");
+		System.out.println("Hi, I'm Edy Segura");
 		System.out.println("Hello World");
 	}
 	
