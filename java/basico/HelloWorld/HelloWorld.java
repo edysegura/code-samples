caabf4891b4dfd01b9dd5a34284ba3e394cf0d56
Edy Segura <edysegura@gmail.com>
2008-10-29 18:09:46 +0000

    Adição do HelloWorld

---
+++ java/basico/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * O menor programa em java é constituído de uma classe com o método main
+ * @author Edy Segura, edy@segura.pro.br
+ *
+ */
+ 
+public class HelloWorld { //declaração da classe
+	//declaração do método main que da início ao programa
+	public static void main (String args[]) {
+		//saída no console
+		System.out.println("Hello World!");
+	}
+}
