638820851f2bf3d75c59444a73954fe74acd2a5c
Edy Segura <edysegura@gmail.com>
2009-03-23 15:06:27 +0000

    Exemplo para limitar textarea.

---
+++ javascript/formularios/limeted-textarea/js/Index.js
@@ -0,0 +1,22 @@
+﻿/**
+ *
+ * Objeto Literal Index
+ * @author: Edy Segura - edy@segura.pro.br
+ *
+ */
+var Index = {
+	
+	init: function() {
+		Index.setLimitToTextArea();
+	},
+	
+	
+	setLimitToTextArea: function() {
+		var textarea = document.getElementById('mytext');
+		Form.setLimitTextArea(textarea);
+	}
+	
+};
+
+//inicialização
+window.onload = Index.init;
---
+++ javascript/formularios/limeted-textarea/js/libs/Form.js
@@ -0,0 +1,46 @@
+﻿/**
+ *
+ * Objeto para manipulação de formulário
+ * @author: Edy Segura - edy@segura.pro.br
+ *
+ */
+var Form = {
+	
+	
+	/**
+	 *
+	 *  Método para configurar a limitação no textarea
+	 *
+	 */
+	setLimitTextArea: function(textarea) {
+		textarea.onmousemove = textarea.onkeydown = textarea.onkeyup = function() {
+			try {
+				var arrayResult = this.className.match(/limit:(\d)+/g);
+				var maxLength   = arrayResult[0].replace(/\D/g, "");
+				Form.limitTextArea(this, maxLength);
+			}
+			catch(e) {
+				alert('Error: ' + e.message);
+			}
+		}
+	},
+	
+	
+	/**
+	 *
+	 *  Método para limitar textarea
+	 *  Chamar os eventos onkeydown e onkeypress e onmouseup
+	 *
+	 */
+	limitTextArea: function(textarea, maxLength, restInfo) {
+		if (textarea.value.length > maxLength) {
+			textarea.value = textarea.value.substring(0, maxLength);
+		}
+		
+		var restInfo = document.getElementById(textarea.id + '-infoLength');
+		if(restInfo) {
+			restInfo.innerHTML = maxLength - textarea.value.length;
+		}
+	}
+	
+};
