ff0b44fcab827f1ec00fa70c3008dff84adfda51
Edy Segura <edysegura@gmail.com>
2010-02-10 13:03:29 +0000

    - Atualização do exemplo document.getElementsByTagName(*)

---
+++ javascript/DOM/getElementsByTagName/js/Index.js
@@ -0,0 +1,32 @@
+﻿/**
+ *
+ * Objeto Literal Index para uso do metodo document.getElementsByTagName("*")
+ * @author: Edy Segura - edy@segura.pro.br
+ *
+ */
+var Index = {
+	
+	init: function() {
+		Index.getAllElements();
+	},
+	
+	
+	getAllElements: function() {
+		var elements = document.getElementsByTagName("*"); //seletor estrela
+		var list     = document.createElement("ol");
+		
+		for(var i=0; i<elements.length; i++) {
+			var item    = document.createElement("li");
+			var element = elements[i];
+			var text    = "<" + element.nodeName.toLowerCase() + " />";
+			item.appendChild(document.createTextNode(text));
+			list.appendChild(item);
+		}
+		
+		document.body.appendChild(list);
+	}
+	
+}
+
+//inicializacao
+window.onload = Index.init;
