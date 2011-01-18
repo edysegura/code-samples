5f4345d50733642f6d9377a45bf77dd14abd2385
Edy Segura <edysegura@gmail.com>
2011-01-18 19:11:43 +0000

    - Atualização do banco de códigos!

--- javascript/tecnicas/animacao/js/Index.js
+++ javascript/tecnicas/animacao/js/Index.js
@@ -4,11 +4,9 @@
  * @author Edy Segura, edy@segura.pro.br
  *
  */
-
 var Index = {
 	
 	count: 0,
-	container: null,
 	
 	init: function() {
 		Index.setAnimate();
@@ -16,31 +14,43 @@ var Index = {
 	
 	
 	setAnimate: function() {
-		var h2 = document.getElementsByTagName("h2")[0];
-		var text = h2.innerHTML;
-		var textConcat = "";
+		var h2 = Index.getH2(),
+		    text = h2.innerHTML,
+		    textConcat = "", charactere;
 		
 		for(var i=0; i<text.length; i++) {
-			textConcat += "<span>" + text.charAt(i) + "</span>";
+			charactere = text.charAt(i);
+			if(/\s+/.test(character)) {
+				textConcat += "<span>" + character + "</span>";
+			}
 		}
 		
 		h2.innerHTML = textConcat;
-		Index.container = h2;
-		
 		window.setInterval(Index.animate, 300);
 	},
 	
 	
 	animate: function() {
-		var h2 = Index.container;
+		var h2 = Index.getH2(),
+		    numChilds = h2.childNodes.length;
 		
 		//animando o conteúdo
-		h2.childNodes[!Index.count ? h2.childNodes.length-1 : Index.count-1].className = "";
+		h2.childNodes[!Index.count ? numChilds-1 : Index.count-1].className = "";
 		h2.childNodes[Index.count].className = "selected";
 		
 		//contador
-		Index.count = ++Index.count % h2.childNodes.length; // n%n=0 volta do incio;
+		Index.count = ++Index.count % numChilds; // n%n=0 volta do incio;
+	},
+	
+	
+	getH2: function() {
+		var h2 = document.getElementsByTagName('h2')[0];
+		if(h2) {
+			return h2;
+		}
+		return null;
 	}
+	
 }
 
 //inicializacao
